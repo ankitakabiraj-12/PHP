84ef2a24ec1b463d45c460bcaf2dbfc09ae4177e
ANKITA KABIRAJ <ankitakabiraj22@gmail.com>
2026-08-31 10:17:53 +0530

    Add PHP script to display user information in a table

---
+++ 10_string_table.php
@@ -0,0 +1,36 @@
+<?php
+$name = "Ankita Kabiraj";
+$age = 19;
+$city = "Kolkata";
+$course = "BCA";
+$college = "Bhawanipur Global Campus";
+$subject = "Web Development";
+$language = "PHP";
+$hobby = "Drawing";
+$country = "India";
+$email = "ankita@example.com";
+$semester = "5th Semester";
+$university = "MAKAUT";
+$favoriteFood = "Biryani";
+$favoritePlace = "Himachal Pradesh";
+$goal = "Software Developer";
+echo "<table border='1'>";
+echo "<tr><th>Field</th><th>Value</th></tr>";
+echo "<tr><td>Name</td><td>$name</td></tr>";
+echo "<tr><td>Age</td><td>$age</td></tr>";
+echo "<tr><td>City</td><td>$city</td></tr>";
+echo "<tr><td>Course</td><td>$course</td></tr>";
+echo "<tr><td>College</td><td>$college</td></tr>";
+echo "<tr><td>Subject</td><td>$subject</td></tr>";
+echo "<tr><td>Programming 
+Language</td><td>$language</td></tr>";
+echo "<tr><td>Hobby</td><td>$hobby</td></tr>";
+echo "<tr><td>Country</td><td>$country</td></tr>";
+echo "<tr><td>Email</td><td>$email</td></tr>";
+echo "<tr><td>Semester</td><td>$semester</td></tr>";
+echo "<tr><td>University</td><td>$university</td></tr>";
+echo "<tr><td>Favorite Food</td><td>$favoriteFood</td></tr>";
+echo "<tr><td>Favorite Place</td><td>$favoritePlace</td></tr>";
+echo "<tr><td>Career Goal</td><td>$goal</td></tr>";
+echo "</table>";
+?>
