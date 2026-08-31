d343e01fba90a5abc4d763777a96b084f2f19390
ANKITA KABIRAJ <ankitakabiraj22@gmail.com>
2026-08-31 10:18:43 +0530

    Add PHP script to display operating system information

---
+++ 11_operating_system.php
@@ -0,0 +1,6 @@
+<?php
+// php_uname() returns information about the operating system
+$os = php_uname();
+// Display the operating system information
+echo "<h2>Operating System Information: " . $os . "</h2>";
+?>
